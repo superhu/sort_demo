3b785a606084ef4172edb8d4a69d6ea7d386441c
huzhichao <569791786@qq.com>
2018-11-17 16:34:37 +0800

    walking file

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,165 @@
 extern crate rand;
 
+use std::fs;
+use std::path::Path;
+
 //use rand::Rng;
 use rand::prelude::*;
+use std::fs::DirEntry;
+use std::io;
+
 //use std::convert::TryInto;
 
 fn main() {
 //    let  arr =  &mut [2,3,1,5,6,4];
-
 //    sort(arr);
-    gen_code();
+
+//    gen_code();
+
+
+    let path = Path::new("D:\\Pictures\\images\\1111");
+
+
+   let _ = visit_dirs(path,&print_filter_dir_entry);
+}
+
+
+fn print_filter_dir_entry(p:&DirEntry){
+
+
+    let filename = &p.file_name();
+    let ff = filename.to_str();
+    match ff {
+        Some(f) => println!("{:?}",f),
+        None => (),
+    }
+
 
 }
 
-#[warn(dead_code)]
-fn sort(arr: &mut [i32])->&mut [i32]{
-    println!("before:{:?}",arr);
+///## 1.数组排序
+#[allow(dead_code)]
+fn sort(arr: &mut [i32]) -> &mut [i32] {
+    println!("before:{:?}", arr);
     let mut min;
-    for i in 0..arr.len(){
-        println!("{}",arr[i]);
+    for i in 0..arr.len() {
+        println!("{}", arr[i]);
         min = i;
-        for j in (i+1)..arr.len(){
-            if arr[j] < arr[min]{
+        for j in (i + 1)..arr.len() {
+            if arr[j] < arr[min] {
                 min = j;
             }
         }
         let temp = arr[i];
         arr[i] = arr[min];
-        arr[min]=temp;
+        arr[min] = temp;
     };
-    println!("after:{:?}",arr);
+    println!("after:{:?}", arr);
     arr
-
 }
 
-
-fn gen_code() -> String{
-
-    let pool1 = (1..36).collect::<Vec<i32>>();
-
-    println!("{:?}",pool1);
-
-    let p1 = &mut gen_num(1,36,5);
-
-    let p2 = &mut gen_num(1,13,2);
+///## 2.超级大乐透摇奖
+#[allow(dead_code)]
+fn gen_code() -> String {
+    let p1 = &mut gen_num(1, 36, 5);
+    let p2 = &mut gen_num(1, 13, 2);
 
     p1.sort();
-
-
     p2.sort();
 
-
-
-    println!("{:?}",p1);
-
-    println!("{:?}",p2);
+    println!("{:?}", p1);
+    println!("{:?}", p2);
     //补零
-    let mut pp1 =vec![];
-    for &mut i in p1{
-        if i <  10 {
+    let mut pp1 = vec![];
+    for &mut i in p1 {
+        if i < 10 {
             pp1.push("0".to_string() + &i.to_string());
-        }else{
+        } else {
             pp1.push("".to_string() + &i.to_string());
         }
     }
-    println!("pp1:{:?}",pp1);
+    println!("pp1:{:?}", pp1);
 
 
-    let mut pp2 =vec![];
-    for &mut i in p2{
-        if i <  10 {
+    let mut pp2 = vec![];
+    for &mut i in p2 {
+        if i < 10 {
             pp2.push("0".to_string() + &i.to_string());
-        }else{
+        } else {
             pp2.push("".to_string() + &i.to_string());
         }
     }
-    println!("pp2：{:?}",pp2);
+    println!("pp2：{:?}", pp2);
 
-    let result = pp1.join(" ").to_string() +"_"+ &pp2.join(" ");
+    let result = pp1.join(" ").to_string() + "-" + &pp2.join(" ");
 
 
-    println!("result：{:?}",result);
+    println!("result：{:?}", result);
     result
-
-
-
 }
 
 
-
-fn gen_num(start:i32,end:i32,size:usize)->Vec<i32>{
+fn gen_num(start: i32, end: i32, size: usize) -> Vec<i32> {
     let mut list1 = Vec::new();
 
 
-    loop{
-        let n1 = rand::thread_rng().gen_range(start,end);
+    loop {
+        let n1 = rand::thread_rng().gen_range(start, end);
         if !list1.contains(&n1) {
             list1.push(n1);
         }
-        if list1.len()==size {
+        if list1.len() == size {
             break;
         }
     }
     list1
 }
+/// ## 3.遍历文件
+#[allow(dead_code)]
+fn list_file(path: &str) {
+    let paths = fs::read_dir(path).unwrap();
 
+
+
+    paths
+        .filter_map(Result::ok)
+        .filter_map(|d| d.path().to_str().and_then(|f| if f.ends_with(".jpg"){Some(d)} else { None }))
+        .for_each(|f| println!("{:?}",f));
+
+//    paths
+//        .filter_map(Result::ok)
+//        .filter_map(|d| d.path().is_dir())
+//        .filter_map(|d| d.path().to_str().and_then(|f| list_file(f);Some(d)));
+
+}
+
+/// ## 3.遍历文件
+fn visit_dirs(dir: &Path, fn_print_path: &Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, fn_print_path)?;
+            } else {
+
+                fn_print_path(&entry);
+            }
+        }
+    }
+    Ok(())
+}
 
 
 
 #[test]
-fn test_sort(){
-    let  arr =  &mut [2,3,1,5,6,4];
+fn test_sort() {
+    let arr = &mut [2, 3, 1, 5, 6, 4];
 
-    assert_eq!([1,2,3,4,5,6],sort(arr))
+    assert_eq!([1, 2, 3, 4, 5, 6], sort(arr))
 }
 
 #[test]
-fn test_gen_code(){
-    println!("{}",gen_code());
+fn test_gen_code() {
+    println!("{}", gen_code());
 }
